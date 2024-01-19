bde06c5337a53e4f5dd01000c12ee4dcca617386
razor6ack <155308268+razor6ack@users.noreply.github.com>
2024-01-19 12:22:37 +0700

    Added scripts to create symlinks to btop, raycast, neofetch, gh, nvim, Autodesk, htop, and iterm2 in ~/.configin install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,27 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #nvim
 ln -s ${BASEDIR}/config/nvim ~/.config/nvim
 
+#btop
+ln -s ${BASEDIR}/config/btop ~/.config/btop
+
+#raycast
+ln -s ${BASEDIR}/config/raycast ~/.config/raycast
+
+#neofetch
+ln -s ${BASEDIR}/config/neofetch ~/.config/neofetch
+
+#gh
+ln -s ${BASEDIR}/config/gh ~/.config/gh
+
+#htop
+ln -s ${BASEDIR}/config/htop ~/.config/htop
+
+#iterm2
+ln -s ${BASEDIR}/config/iterm2 ~/.config/iterm2
+
+#Autodesk
+ln -s ${BASEDIR}/config/Autodesk ~/.config/Autodesk
+
 #zshrc
 ln -s ${BASEDIR}/zshrc ~/.zshrc
 
