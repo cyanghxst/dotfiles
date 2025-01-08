deff47d33ed66f4b3ce643459cf4619be57eb50c
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-01-07 23:55:48 -0800

    feat(scripts): add tmux-init.sh

---
+++ config/scripts/tmux-init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -n "$TMUX" ]; then
+    exit 0
+fi
+
+sessions=("main" "lcpp" "1160" "1181" "1280")
+
+directories=(
+    "$HOME/git/repos/dotfiles/"
+    "$HOME/code/learn-cpp"
+    "$HOME/code/langara/cpsc-1160"
+    "$HOME/code/langara/cpsc-1181"
+    "$HOME/code/langara/cpsc-1280"
+)
+
+for i in "${!sessions[@]}"; do
+    session="${sessions[$i]}"
+    dir="${directories[$i]}"
+
+    tmux has-session -t "$session" 2>/dev/null
+
+    if [ $? != 0 ]; then
+        tmux new-session -d -s "$session" "cd ~; exec zsh"
+        tmux new-window -t "$session:1" "cd $dir; exec zsh"
+        tmux send-keys -t "$session:1" "git lol" C-m
+    fi
+done
+
+tmux attach -t main
