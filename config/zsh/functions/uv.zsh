b4da1212c52ee0895bda75e2acf962d56e0cad7d
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-08-08 20:10:28 -0700

    refactor(functions): improve uv.zsh by hashing

--- config/zsh/functions/uv.zsh
+++ config/zsh/functions/uv.zsh
@@ -1,13 +1,26 @@
-# Set up UV to use a venv directory in ~/.cache/uv-venv, based on the project
-# root directory or the current working directory if no project can be found.
+# Custom uv wrapper to use a centralized venv location under ~/.cache/uv-venv
 #
-# Courtesy Hawk777: https://github.com/astral-sh/uv/issues/1495#issuecomment-2910199607
+# Modified from Hawk777's idea:
+# https://github.com/astral-sh/uv/issues/1495#issuecomment-2910199607
+
 uv() {
-    local uv_project_root=$(/usr/bin/uv -v python find 2>&1 >/dev/null | sed -ne '/Found project root: `/{s/^.*Found project root: `\(.*\)`$/\1/;p}')
-    if [[ -n $uv_project_root ]]; then
-        export UV_PROJECT_ENVIRONMENT=~/.cache/uv-venv/$uv_project_root
+    local uv_project_root hash
+
+    uv_project_root=$(
+        /usr/bin/uv -v python find 2>&1 >/dev/null | \
+            sed -n -e '/Found project root: `/ {
+                s/^.*Found project root: `\(.*\)`$/\1/
+                p
+            }'
+    )
+
+    if [[ -n "$uv_project_root" ]]; then
+        hash=$(echo "$uv_project_root" | sha256sum | cut -c1-8) # use hashing for unique id
     else
-        export UV_PROJECT_ENVIRONMENT=~/.cache/uv-venv/$PWD
+        hash=$(echo "$PWD" | sha256sum | cut -c1-8)
     fi
+
+    export UV_PROJECT_ENVIRONMENT="$HOME/.cache/uv-venv/$hash"
+
     command uv "$@"
 }
