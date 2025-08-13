a0f58d16e970ae5dcb63beb859a7526d50fe5bd2
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-08-13 14:24:59 -0700

    fix(functions): use (@f) instead of readarray for zsh

--- config/zsh/functions/exercism.zsh
+++ config/zsh/functions/exercism.zsh
@@ -1,7 +1,8 @@
 exercism () {
-    local out
-    readarray -t out < <(command exercism "$@")
+    local out=("${(@f)$(command exercism "$@")}")
+
     printf '%s\n' "${out[@]}"
+
     if [[ $1 == "download" && -d "${out[-1]}" ]]; then
         cd "${out[-1]}" || return 1
     fi
