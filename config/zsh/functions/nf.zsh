4d325ffc169de2e16376f8a8f9c19538b804b1e3
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-08-15 00:03:54 -0700

    fix(functions): correct return value for nf.zsh

--- config/zsh/functions/nf.zsh
+++ config/zsh/functions/nf.zsh
@@ -1,14 +1,14 @@
 nf() {
     local file dir base
 
-    file="$(fzf --exit-0)" || return
-    [[ -n "$file" ]] || return
+    file="$(fzf --exit-0)" || return 0
+    [[ -n "$file" ]] || return 1
 
     dir="${file%/*}"
     base="${file##*/}"
 
     [[ "$dir" == "$base" ]] && dir="."
 
-    cd -- "$dir" || return
+    cd -- "$dir" || return 1
     nvim -- "$base"
 }
