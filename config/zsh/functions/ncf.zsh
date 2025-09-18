6170d750d47a343a070a22db402c7201fd9a1b5d
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-09-18 14:47:39 -0700

    feat(functions): add ncf.zsh

---
+++ config/zsh/functions/ncf.zsh
@@ -0,0 +1,14 @@
+ncf() {
+    local file dir base
+
+    file="$(fzf --exit-0)" || return 0
+    [[ -n "$file" ]] || return 1
+
+    dir="${file%/*}"
+    base="${file##*/}"
+
+    [[ "$dir" == "$base" ]] && dir="."
+
+    cd -- "$dir" || return 1
+    nvim -- "$base"
+}
