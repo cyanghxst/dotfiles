61812717735e0b735ec95a359949d38bcbecfb61
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-09-18 15:08:29 -0700

    fix(functions): correct nf behavior

--- config/zsh/functions/nf.zsh
+++ config/zsh/functions/nf.zsh
@@ -1,13 +1,5 @@
 nf() {
-    local file dir base
-
-    file="$(fzf --exit-0)" || return 0
+    local file="$(fzf --exit-0)" || return 0
     [[ -n "$file" ]] || return 1
-
-    dir="${file%/*}"
-    base="${file##*/}"
-
-    [[ "$dir" == "$base" ]] && dir="."
-
-    nvim -- "$base"
+    nvim -- "$file"
 }
