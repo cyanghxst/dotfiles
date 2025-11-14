96f564be760c91e6d47ad69802f0a28e57099c96
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-11-14 01:12:10 -0800

    feat(functions): exclude .class and dotfiles in nf.zsh

--- config/zsh/functions/nf.zsh
+++ config/zsh/functions/nf.zsh
@@ -1,7 +1,7 @@
 nf() {
     local file
 
-    file="$(fzf --exit-0 --exclude='*.class')" || return 0
+    file="$(find . -not -path '*/.*' -type f -not -name '*.class' | fzf --exit-0)" || return 0
     [[ -n "$file" ]] || return 1
 
     nvim -- "$file"
