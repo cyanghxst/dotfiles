e63cc8898c07fa8a4920cc898e42bf36b4cddd72
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-02-16 13:05:24 -0800

    feat(ccom): support separate compilation for C and C++

--- config/zsh/functions/ccom.zsh
+++ config/zsh/functions/ccom.zsh
@@ -1,24 +1,57 @@
 ccom() {
     local output=""
-    local files=()
+    local srcs=()
+    local objs=()
+    local has_cpp=0
 
-    while [[ $# -gt 0 ]]; do
+    while (( $# )); do
         case "$1" in
             -o)
                 output="$2"
                 shift 2
                 ;;
             *)
-                files+=("$1")
+                srcs+=("$1")
                 shift
                 ;;
         esac
     done
 
-    if [[ -z "$output" ]]; then
-        output="${files[1]%.*}"  # Use the name of the first file as default
-    fi
+    (( ${#srcs} == 0 )) && {
+        echo "ccom: no input files"
+        return 1
+    }
 
-    g++ -std=c++20 -O0 "${files[@]}" -o "$output" -Wall -Werror -Wextra -Weffc++ -Wconversion -Wsign-conversion -pedantic-errors
-    # g++ -std=c++17 -O2 -o "${files[@]}" "$output" -Wall }
+    # zsh arrays are 1-based
+    if [[ -z $output ]]; then
+        output="${srcs[1]%.*}"
+    fi
+    [[ -z $output ]] && output="a.out"
+
+    for src in "${srcs[@]}"; do
+        obj="${src%.*}.o"
+        case "$src" in
+            *.c)
+                clang -std=c17 -O0 -c "$src" -o "$obj" \
+                    -Wall -Wextra -Werror -Wconversion -Wsign-conversion -pedantic-errors || return 1
+                ;;
+            *.cpp|*.cc|*.cxx)
+                has_cpp=1
+                clang++ -std=c++20 -O0 -c "$src" -o "$obj" \
+                    -Wall -Wextra -Werror -Wconversion -Wsign-conversion \
+                    -Weffc++ -pedantic-errors || return 1
+                ;;
+            *)
+                echo "ccom: unsupported file type: $src"
+                return 1
+                ;;
+        esac
+        objs+=("$obj")
+    done
+
+    if (( has_cpp )); then
+        clang++ "${objs[@]}" -o "$output"
+    else
+        clang "${objs[@]}" -o "$output"
+    fi
 }
