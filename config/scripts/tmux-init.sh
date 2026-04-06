0ee15611ee41913fec989b6e47e9be0b06f48ab0
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-04-06 15:26:15 -0700

    fix(scripts): update tmux-init.sh

--- config/scripts/tmux-init.sh
+++ config/scripts/tmux-init.sh
@@ -4,18 +4,19 @@ if [[ -n "$TMUX" ]]; then
     exit 0
 fi
 
-sessions=("main" "obsidian" "homelab" "website" "cpsc-213" )
+sessions=("main" "obsidian" "homelab" "website" "hackathon")
 
 directories=(
     "$HOME/git/repos/dotfiles/"
     "$HOME/git/repos/obsidian/"
     "$HOME/git/repos/homelab/"
-    "$HOME/git/repos/website-erudite/"
-    "$HOME/code/ubc/cpsc-213-collab/"
+    "$HOME/git/repos/website/"
+    "$HOME/git/repos/pe-hackathon/"
 )
 
-vault_directory="$HOME/git/repos/vault-2/"
+vault_directory="$HOME/git/repos/vault/"
 nvim_directory="$HOME/git/repos/nvim/"
+homebrew_tap_directory="$HOME/git/repos/homebrew-cyanghxst/"
 
 for i in "${!sessions[@]}"; do
     session="${sessions[$i]}"
@@ -30,31 +31,50 @@ for i in "${!sessions[@]}"; do
         tmux new-window -t "$session" "cd $dir; exec zsh"
         tmux send-keys -t "$session" "ls" C-m
 
+        # main: cd to nvim
         if [[ $i -eq 0 && -d "$nvim_directory" ]]; then
             tmux new-window -t "$session" "cd $nvim_directory; exec zsh"
             tmux send-keys -t "$session" "ls" C-m
+
+            tmux new-window -t "$session" "cd $homebrew_tap_directory; exec zsh"
+            tmux send-keys -t "$session" "ls" C-m
         fi
 
+        # obsidian: cd to vault
         if [[ $i -eq 1 && -d "$vault_directory" ]]; then
             tmux new-window -t "$session" "cd $vault_directory; exec zsh"
             tmux send-keys -t "$session" "ls" C-m
         fi
 
+        # homelab: ssh into remote
         if [[ $i -eq 2 ]]; then
             tmux new-window -t "$session" "cd $dir; exec zsh"
             tmux send-keys -t "$session" "ssh proxmox" C-m
+
+            tmux new-window -t "$session" "cd $dir; exec zsh"
+            tmux send-keys -t "$session" "ssh opnsense" C-m
         fi
 
+        # website: open dev env
         if [[ $i -eq 3 ]]; then
             tmux new-window -t "$session" "cd $dir; exec zsh"
             tmux send-keys -t "$session" "npm run dev" C-m
+
+            tmux new-window -t "$session" "cd $dir; exec zsh"
+            tmux send-keys -t "$session" "opencode web" C-m
         fi
 
-        # if [[ $i -eq 3 || $i -eq 4 ]]; then
-        #     tmux new-window -t "$session" "cd $dir; exec zsh"
-        #     tmux send-keys -t "$session" "ls" C-m
-        #     tmux send-keys -t "$session" "nf" C-m
-        # fi
+        # hackathon: ssh into kube
+        if [[ $i -eq 4 ]]; then
+            tmux new-window -t "$session" "cd $dir; exec zsh"
+            tmux send-keys -t "$session" "ssh optiplex-node1" C-m
+
+            tmux new-window -t "$session" "cd $dir; exec zsh"
+            tmux send-keys -t "$session" "ssh optiplex-node2" C-m
+
+            tmux new-window -t "$session" "cd $dir; exec zsh"
+            tmux send-keys -t "$session" "ssh optiplex-node3" C-m
+        fi
     fi
 done
 
